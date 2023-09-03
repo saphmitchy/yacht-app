b39e6a8fbae2dfc8f891cfe490c6b5dfec3215a6
saphmitchy <nakamichi-kohei135@g.ecc.u-tokyo.ac.jp>
2023-09-03 09:48:49 +0900

    add main logics

--- __tests__/yacht.test.tsx
+++ __tests__/yacht.test.tsx
@@ -1,5 +1,5 @@
 import { test, expect } from "@jest/globals";
-import { box } from "../app/yacht";
+import { Yacht, box } from "../app/yacht";
 
 test("ones", () => {
     const f = box[0].func;
@@ -54,17 +54,17 @@ test("fourOfAKind", () => {
     expect(f([5, 4, 5, 5, 5])).toBe(24);
     expect(f([1, 1, 2, 1, 1])).toBe(6);
     expect(f([1, 1, 1, 1, 1])).toBe(5);
-    expect(f([2, 1, 2, 1, 1])).toBeNull();
-    expect(f([2, 1, 2, 3, 3])).toBeNull();
+    expect(f([2, 1, 2, 1, 1])).toBeUndefined();
+    expect(f([2, 1, 2, 3, 3])).toBeUndefined();
 })
 
 test("fullHouse", () => {
     const f = box[8].func;
     expect(f([5, 4, 5, 5, 4])).toBe(23);
     expect(f([1, 1, 4, 1, 4])).toBe(11);
-    expect(f([1, 1, 4, 1, 1])).toBeNull();
+    expect(f([1, 1, 4, 1, 1])).toBeUndefined();
     expect(f([1, 1, 1, 1, 1])).toBe(5);
-    expect(f([1, 2, 2, 3, 1])).toBeNull();
+    expect(f([1, 2, 2, 3, 1])).toBeUndefined();
 })
 
 test("smallStraight", () => {
@@ -72,24 +72,65 @@ test("smallStraight", () => {
     expect(f([5, 4, 3, 2, 4])).toBe(15);
     expect(f([1, 3, 4, 2, 4])).toBe(15);
     expect(f([5, 4, 2, 3, 6])).toBe(15);
-    expect(f([6, 1, 3, 2, 5])).toBeNull();
-    expect(f([1, 2, 2, 5, 6])).toBeNull();
+    expect(f([6, 1, 3, 2, 5])).toBeUndefined();
+    expect(f([1, 2, 2, 5, 6])).toBeUndefined();
 })
 
 test("largeStraight", () => {
     const f = box[10].func;;
     expect(f([5, 1, 3, 2, 4])).toBe(30);
     expect(f([5, 4, 2, 3, 6])).toBe(30);
-    expect(f([1, 3, 4, 2, 4])).toBeNull();
-    expect(f([6, 1, 3, 2, 5])).toBeNull();
-    expect(f([1, 2, 2, 5, 6])).toBeNull();
+    expect(f([1, 3, 4, 2, 4])).toBeUndefined();
+    expect(f([6, 1, 3, 2, 5])).toBeUndefined();
+    expect(f([1, 2, 2, 5, 6])).toBeUndefined();
 })
 
 test("yacht", () => {
     const f = box[11].func;;
     expect(f([4, 4, 4, 4, 4])).toBe(50);
     expect(f([1, 1, 1, 1, 1])).toBe(50);
-    expect(f([4, 3, 4, 4, 4])).toBeNull();
-    expect(f([1, 1, 3, 2, 5])).toBeNull();
-    expect(f([1, 2, 3, 5, 6])).toBeNull();
+    expect(f([4, 3, 4, 4, 4])).toBeUndefined();
+    expect(f([1, 1, 3, 2, 5])).toBeUndefined();
+    expect(f([1, 2, 3, 5, 6])).toBeUndefined();
+})
+
+test("get_round", () => {
+    let y = new Yacht();
+    expect(y.get_round()).toBe(0);
+    y.points[0] = 4;
+    y.points[11] = 4;
+    expect(y.get_round()).toBe(2);
+})
+
+test("throw_dice", () => {
+    const y = new Yacht();
+    let z = y.throw_dice()!;
+    expect(z.turn).toBe(1);
+    expect(z.points === y.points).toBe(true);
+    expect(z.dice === y.dice).toBe(false);
+    z = z.throw_dice()!;
+    expect(z.turn).toBe(2);
+    z = z.throw_dice()!;
+    expect(z.turn).toBe(3);
+    expect(z.throw_dice()).toBeUndefined();
+})
+
+test("lock_dice", () => {
+    const y = new Yacht();
+    const z = y.lock_dice(2);
+    expect(z.dice[2].locked).toBe(true);
+    expect(z.points === y.points).toBe(true);
+    expect(z.dice === y.dice).toBe(false);
+    expect(z.turn === y.turn).toBe(true);
+})
+
+test("fill_box", () => {
+    const y = new Yacht();
+    y.turn = 2;
+    const z = y.fill_box(0)!;
+    expect(z.points === y.points).toBe(false);
+    expect(z.dice === y.dice).toBe(true);
+    expect(z.turn === y.turn).toBe(false);
+    expect(z.points[0]).toBe(5);
+    expect(z.fill_box(0)).toBeUndefined();
 })
--- app/yacht.tsx
+++ app/yacht.tsx
@@ -1,3 +1,5 @@
+import { randomInt } from "crypto";
+
 export type Dice = Array<number>;
 
 function count(d: Dice, n: number) {
@@ -29,27 +31,27 @@ function sixes(d: Dice) {
 }
 
 function fullHouse(d: Dice) {
-    let e = d.map(x => x);
+    let e = [...d];
     e.sort();
     if (e[0] == e[1] && e[3] == e[4] && (e[1] == e[2] || e[2] == e[3])) {
         return d.reduce((a, b) => a + b);
     } else {
-        return null;
+        return undefined;
     }
 }
 
 function fourOfAKind(d: Dice) {
-    let e = d.map(x => x);
+    let e = [...d];
     e.sort();
     if (e[1] == e[2] && e[2] == e[3] && (e[0] == e[1] || e[3] == e[4])) {
         return d.reduce((a, b) => a + b);
     } else {
-        return null;
+        return undefined;
     }
 }
 
 function smallStraight(d: Dice) {
-    let e = d.map(x => x);
+    let e = [...d];
     e.sort();
     let a = 0;
     let b = 0;
@@ -63,17 +65,17 @@ function smallStraight(d: Dice) {
     if ((a == 1 && b == 3) || (a == 0 && b == 4)) {
         return 15;
     } else {
-        return null;
+        return undefined;
     }
 }
 
 function largeStraight(d: Dice) {
-    let e = d.map(x => x);
+    let e = [...d];
     e.sort();
     if (e[0] + 1 == e[1] && e[1] + 1 == e[2] && e[2] + 1 == e[3] && e[3] + 1 == e[4]) {
         return 30;
     } else {
-        return null;
+        return undefined;
     }
 }
 
@@ -81,7 +83,7 @@ function yacht(d: Dice) {
     if (d[0] == d[1] && d[1] == d[2] && d[2] == d[3] && d[3] == d[4]) {
         return 50;
     } else {
-        return null;
+        return undefined;
     }
 }
 
@@ -103,3 +105,110 @@ export const box = [
     { id: "largeStraight", func: largeStraight },
     { id: "yacht", func: yacht },
 ]
+
+export class Yacht {
+    public readonly dice: {
+        value: number,
+        locked: boolean,
+    }[];
+    public readonly points: Array<number | undefined>;
+    public readonly turn: number;
+
+    constructor() {
+        this.dice = new Array(5);
+        this.points = new Array(box.length);
+        this.turn = 0;
+        this.points.fill(undefined);
+        for (let i = 0; i < this.dice.length; i++) {
+            this.dice[i] = { value: 1, locked: false };
+        }
+    }
+
+    get_round(): number {
+        return this.points.map<number>(x => (x == undefined) ? 0 : 1).reduce((a, b) => a + b);
+    }
+
+    throw_dice() {
+        if (3 <= this.turn ||
+            this.get_round() == this.points.length ||
+            !this.dice.some(x => !x.locked)) {
+            return undefined;
+        }
+        let res: Yacht = Object.assign(
+            Object.create(Yacht.prototype),
+            this,
+            {
+                dice: [...this.dice],
+                turn: this.turn + 1,
+            }
+        );
+        for (let i = 0; i < this.dice.length; i++) {
+            res.dice[i] = {
+                ...this.dice[i],
+                value: randomInt(1, 7),
+            }
+        }
+        return res;
+    }
+
+    lock_dice(index: number): Yacht {
+        let dice = [...this.dice];
+        dice[index] = {
+            ...this.dice[index],
+            locked: true,
+        }
+        return Object.assign(
+            Object.create(Yacht.prototype),
+            this,
+            {
+                dice,
+            }
+        );
+    }
+
+    unlock_dice(index: number): Yacht {
+        let dice = [...this.dice];
+        dice[index] = {
+            ...this.dice[index],
+            locked: false,
+        }
+        return Object.assign(
+            Object.create(Yacht.prototype),
+            this,
+            {
+                dice,
+            }
+        )
+    }
+
+    fill_box(index: number): Yacht | undefined {
+        if (this.points[index] != undefined) {
+            return undefined;
+        }
+        let res = Object.assign(
+            Object.create(Yacht.prototype),
+            this,
+            {
+            points: [...this.points],
+            turn: 0,
+        });
+        res.points[index] = box[index].func(this.dice.map(x => x.value));
+        if (res.points[index] == undefined) {
+            res.points[index] = 0;
+        }
+        return res;
+    }
+
+    calc_score() {
+        const f = (a: number | undefined, b: number | undefined) => (a == undefined ? 0 : a) + (b == undefined ? 0 : b);
+        const upper = this.points.slice(0, 6).reduce(f)!;
+        const lower = this.points.slice(6, 12).reduce(f)!;
+        const boanus = (upper >= 63) ? 35 : 0;
+        return {
+            upper,
+            lower,
+            boanus,
+            total: upper + lower + boanus,
+        }
+    }
+}
